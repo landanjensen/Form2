09807915368622ac9cc25d4eab1d09666b821017
landan <landantjensen@gmail.com>
2022-12-06 09:59:44 -0700

    Created a form to see how compatible I am with people taking a quiz.

---
+++ functions.js
@@ -0,0 +1,19 @@
+var buttonThing = document.getElementById("button");
+buttonThing.onclick = putToast
+
+
+
+
+function putToast(){
+
+    window.alert("We are not compatible :(");
+
+
+}
+
+function stylisticChange(){
+
+    var textBox = document.getElementById("something");
+    textBox.style.backgroundColor = "red"
+
+}
